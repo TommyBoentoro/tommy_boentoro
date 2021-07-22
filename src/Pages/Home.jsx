d095c29640dc484da321c3f228edd8d3ffbb0611
tommybun97 <tommybun97@gmail.com>
2021-07-22 20:56:05 +0700

    Day-1

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -9,11 +9,16 @@ function Home(){
         <div className="bg">
             <div className="container">
                 <div className="navbar px-0 fontKanit">
-                    <div>
+                    <div className="navTommy">
                         Tommy Boentoro
                     </div>
-                    <div>
-                        About.
+                    <div className="d-flex">
+                        <div className="navAbout">
+                            About
+                        </div>
+                        <div>
+                            .
+                        </div>
                     </div>
                 </div>
 
@@ -54,9 +59,48 @@ function Home(){
                     </div>
                 </div>
 
-                <div className="gambarHome">
-                    <img src={gambarHome} alt="" />
+                <div className="border" style={{marginTop:"43px"}}>
+                    <img src={gambarHome} className="gambarhome" alt="" style={{width:"100%"}}/>
                 </div>
+
+                <div className="fontKanit">
+                    <div className="indonesian d-flex justify-content-center">
+                        Indonesian
+                    </div>
+                    <div className="d-flex justify-content-center indonesian2 fontPlay">
+                        Full Stack Developer
+                    </div>
+                </div>
+
+                <div className="d-flex sectionWelcome">
+                    <div className="col-2 px-0">
+                        1
+                    </div>
+                    <div className="col-4 fontPlay px-0">
+                        <div className="welcome">
+                            Welcome.
+                        </div>
+                        <div className="selamatDatang">
+                            Selamat Datang !
+                        </div>
+                    </div>
+                    <div className="col-5 px-0">
+                        <div className="bodyWelcome fontKanit">
+                            Lorem ipsum dolor sit amet,consectetur adipiscing elit. 
+                            In porttitor in ligula eget placerat. Nunc odio nulla, 
+                            sollicitudin at diam non, pulvinar facilisis sapien. 
+                            Aenean vel porttitor metus. 
+                            Morbi laoreet turpis sit amet magna varius facilisis. 
+                            Vivamus euismod dui ac nisi dapibus, 
+                            sed malesuada urna tincidunt. Integer suscipit augue neci.
+                        </div>
+                    </div>
+                    <div className="col-1 px-0">
+                        5
+                    </div>
+                </div>
+
+                
             </div>
         </div>
     )
